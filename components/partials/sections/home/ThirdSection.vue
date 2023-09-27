f82c89ac53fb0ce5a5149aa275731fe6083011be
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-09-27 01:59:02 +0100

    third section

---
+++ components/partials/sections/home/ThirdSection.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class=" w-full relative py-10 lg:pl-8 lg:pr-20 pl-4 pr-4 space-y-10 md:space-y-16 ">
+      <p class=" text-blu-ascital font-normal text-[2rem] md:text-[3rem] lg:text-[3.375rem] leading-[1.2] max-w-[63.8125rem]">
+        Offriamo soluzioni complete, <br>
+        con un reale  <span class=" text-verde-ascital">vantaggio competitivo</span>.
+      </p>
+
+      <div class=" w-full flex lg:flex-row flex-col-reverse items-start   lg:items-end lg:justify-between">
+            <div class=" w-full lg:max-w-[20rem] pt-10 lg:pt-0">
+                <div class=" flex flex-col items-start space-y-4">
+                    <span class=" text-testi font-inter text-2xl">Vuoi saperne di più?</span>
+                    <button class=" text-center text-verde-ascital text-2xl font-normal rounded-full bg-blu-ascital max-w-[19.75rem] w-full h-[4.6875rem] duration-200 hover:scale-105">
+                        Richiedi info
+                    </button>
+                </div>
+            </div>
+
+            <div class=" w-full lg:max-w-[49.625rem]">
+                <div class=" w-full flex flex-col items-start space-y-8 md:space-y-14">
+                    <div class=" w-full flex sm:flex-row flex-col space-y-4 sm:space-y-0 items-start sm:justify-between pb-14 border-b border-verde-ascital">
+                        <div class=" flex items-baseline space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 30 24" fill="none">
+                                  <path d="M0 10.38V13.38H24L15.75 21.63L17.88 23.76L29.76 11.88L17.88 0L15.75 2.13L24 10.38H0Z" fill="#373D3F"/>
+                            </svg>
+                            <span class=" text-[2rem] text-testi font-normal">Servizi IT</span>
+                        </div>
+                        <p class=" sm:max-w-[23.9375rem] w-full text-testi text-base font-normal">
+                            Progettazione e realizzazione di soluzioni software per qualsiasi ambiente. Progettazione e realizzazione di soluzioni software per qualsiasi ambiente. Progettazione e realizzazione.
+                        </p>
+                    </div>
+                    <div class=" w-full flex sm:flex-row flex-col space-y-4 sm:space-y-0 items-start sm:justify-between pb-14 border-b border-verde-ascital">
+                        <div class=" flex items-baseline space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 30 24" fill="none">
+                                  <path d="M0 10.38V13.38H24L15.75 21.63L17.88 23.76L29.76 11.88L17.88 0L15.75 2.13L24 10.38H0Z" fill="#373D3F"/>
+                            </svg>
+                            <span class=" text-[2rem] text-testi font-normal">Integrazione sistemi complessi</span>
+                        </div>
+                        <p class=" sm:max-w-[23.9375rem] w-full text-testi text-base font-normal">
+                            Dalla realizzazione di reti LAN e WAN, dalle soluzioni per il monitoraggio del traffico alla realizzazione e di impianti di videosorveglianza e controllo accessi, siamo in grado di gestire tutte le casistiche.
+                        </p>
+                    </div>
+                     <div class=" w-full flex sm:flex-row flex-col space-y-4 sm:space-y-0 items-start sm:justify-between pb-14 border-b border-verde-ascital">
+                        <div class=" flex items-baseline space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 30 24" fill="none">
+                                  <path d="M0 10.38V13.38H24L15.75 21.63L17.88 23.76L29.76 11.88L17.88 0L15.75 2.13L24 10.38H0Z" fill="#373D3F"/>
+                            </svg>
+                            <span class=" text-[2rem] text-testi font-normal">Cyber security</span>
+                        </div>
+                        <p class=" sm:max-w-[23.9375rem] w-full text-testi text-base font-normal">
+                            Gestione dei Processi, Policy, e Strumenti che consentono alle funzionalità di Security di definire,
+                            concretizzare, monitorare e rappresentare al Management gli interventi di mitigazione dei Rischi
+                            e di Compliance.
+                        </p>
+                    </div>
+                     <div class=" w-full flex sm:flex-row flex-col space-y-4 sm:space-y-0 items-start sm:justify-between pb-14 border-b border-verde-ascital">
+                        <div class=" flex items-baseline space-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 30 24" fill="none">
+                                  <path d="M0 10.38V13.38H24L15.75 21.63L17.88 23.76L29.76 11.88L17.88 0L15.75 2.13L24 10.38H0Z" fill="#373D3F"/>
+                            </svg>
+                            <span class=" text-[2rem] text-testi font-normal">Education</span>
+                        </div>
+                        <p class=" sm:max-w-[23.9375rem] w-full text-testi text-base font-normal">
+                            Formazione - Lead Auditor ISO training, ISO 9001, 27001, 20000, 22301
+                            Convenzioni aperte con Università: Sapienza, Tor Vergata e Roma TRE
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+      </div>
+      
+    </div>
+</template>
+
