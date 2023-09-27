28b2b1f26dd155890ca2454df2b53e8a51cd105d
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-09-27 15:12:49 +0100

    fixes

--- components/partials/sections/home/HeroSection.vue
+++ components/partials/sections/home/HeroSection.vue
@@ -2,7 +2,7 @@
     <div class=" w-full h-screen bg-black relative overflow-hidden">
         <!-- Video -->
 
-         <video autoplay muted src="/assets/home/ascital-hero.mov" class=" object-cover w-full h-full"></video>
+         <video  autoplay muted :src="videosrc" class=" object-cover w-full h-full"></video>
 
         <!-- text -->
         <div class=" absolute sm:left-10 left-4 bottom-10 sm:bottom-20 max-w-[59.375rem] w-full ">
@@ -40,7 +40,9 @@
 
 <script setup>
  const index = ref(0);
+ const videosrc = ref('/assets/home/herovideo.mov')
  onMounted(() => {
+    loaded.value = true
     setInterval(()=> {
         index.value++
         if(index.value == 3){
